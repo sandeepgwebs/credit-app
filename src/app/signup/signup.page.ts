b971e96675f753ed0d58748a0bf4b3ad5dfd7c02
Vijay <vijay.gwebs@gmail.com>
2019-06-24 17:55:32 +0530

    initial commit to vijay

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.page.html',
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
-
-  constructor() { }
+  rForm: FormGroup;
+  constructor(public fb: FormBuilder) { 
+    this.rForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+  });
+  }
+  signup(form){
+    console.log(form);
+  }
 
   ngOnInit() {
   }
